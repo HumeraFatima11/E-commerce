0117e801a9b617f5d2476ad5f5ffa85b80ffc8c4
HumeraFatima11 <masoodkaleem999@gmail.com>
2024-01-17 13:00:12 +0530

    UPDATE

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// search
+let search = document.querySelector(".searchbar");
+document.querySelector('#search').onclick = () => {
+    search.classList.toggle('active');
+    favorite.classList.remove('active');
+    checkout.classList.remove('active');
+    login.classList.remove('active');
+}
+
+// favourite
+let favorite = document.querySelector(".favorite");
+document.querySelector('#favorite').onclick = () => {
+    favorite.classList.toggle('active');
+    search.classList.remove('active');
+    checkout.classList.remove('active');
+    login.classList.remove('active');
+
+}
+
+// checkout
+let checkout = document.querySelector(".checkout");
+document.querySelector('#checkout').onclick = () => {
+    checkout.classList.toggle('active');
+    search.classList.remove('active');
+    favorite.classList.remove('active');
+    login.classList.remove('active');
+}
+
+// login
+let login = document.querySelector(".login");
+document.querySelector('#login').onclick = () => {
+    login.classList.toggle('active');
+    search.classList.remove('active');
+    favorite.classList.remove('active');
+    checkout.classList.remove('active');
+}
+
+window.onscroll = () => {
+    search.classList.remove('active');
+    favorite.classList.remove('active');
+    checkout.classList.remove('active');
+    login.classList.remove('active');
+
+}
